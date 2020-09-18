4c696e24c161dd6e1d00ae847138a7b8c77f6b09
nathanmailhiot <nathanjm12@outlook.com>
2020-09-18 13:31:17 -0400

    Home layout and picture

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function Home() {
   return (
     <div className="cube__face cube__face--home">
-      <h1>Home</h1>
+      <div className="home">
+        <div className="home__grid">
+          <h1 className="home__heading">Home Heading</h1>
+          <p className="home__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            elementum eleifend lorem, id consequat sem pretium non. Nam eleifend
+            mollis nibh tincidunt ultrices. Aenean condimentum augue a turpis
+            viverra, ut aliquam nibh iaculis. Aenean id ex varius, eleifend odio
+            ut, efficitur massa.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

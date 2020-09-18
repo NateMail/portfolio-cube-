f9fe9e43cec8ea16551170146c3d98a1071f617e
nathanmailhiot <nathanjm12@outlook.com>
2020-09-18 13:01:01 -0400

    Nav layout/basic styling

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,34 +7,43 @@ export default function Nav(props) {
 
   return (
     <div className="nav">
+      <div className="nav__logo"></div>
       <ul className="nav__list">
         <li className="nav__item">
-          <button onClick={onChange} value="home">
+          <button className="nav__btn" onClick={onChange} value="home">
             Home
           </button>
         </li>
         <li className="nav__item">
-          <button onClick={onChange} value="about">
+          <button className="nav__btn" onClick={onChange} value="about">
             About
           </button>
         </li>
         <li className="nav__item">
-          <button onClick={onChange} value="skills">
+          <button className="nav__btn" onClick={onChange} value="skills">
             Skills
           </button>
         </li>
         <li className="nav__item">
-          <button onClick={onChange} value="personalProjects">
+          <button
+            className="nav__btn"
+            onClick={onChange}
+            value="personalProjects"
+          >
             Personal Projects
           </button>
         </li>
         <li className="nav__item">
-          <button onClick={onChange} value="professionalProjects">
+          <button
+            className="nav__btn"
+            onClick={onChange}
+            value="professionalProjects"
+          >
             Professional Projects
           </button>
         </li>
         <li className="nav__item">
-          <button onClick={onChange} value="contact">
+          <button className="nav__btn" onClick={onChange} value="contact">
             Contact
           </button>
         </li>

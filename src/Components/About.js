2dfa8fd53b44cbd8bc54f76c170d01b94169062e
nathanmailhiot <nathanjm12@outlook.com>
2020-09-21 12:18:57 -0400

    About layout

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function About() {
   return (
     <div className="cube__face cube__face--about">
-      <h1>About</h1>
+      <div className="about">
+        <h1 className="about__heading">Nate Mailhiot</h1>
+        <p className="about__text">
+          I'm a software engineer from the Boston area. I previously have a
+          background working with special needs adults. I have completed a
+          Software Engineering bootcamp at General Assembly. I started my coding
+          journey back in October 2018. I started taking some Python classes
+          online and then decided to make it a career. I started at General
+          Assembly in March 2019 and finished in June 2019.
+        </p>
+        <div className="about__img"></div>
+      </div>
     </div>
   );
 }

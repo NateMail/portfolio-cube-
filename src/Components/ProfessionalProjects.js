4ff821f2b7e140e3e6296d5cf37fba1f8ce102f3
nathanmailhiot <nathanjm12@outlook.com>
2020-09-21 13:46:02 -0400

    Project layouts

--- src/Components/ProfessionalProjects.js
+++ src/Components/ProfessionalProjects.js
@@ -3,7 +3,39 @@ import React from "react";
 export default function ProfessionalProjects() {
   return (
     <div className="cube__face cube__face--professionalProjects">
-      <h1>Professional Projects</h1>
+      <div className="projectsGrid">
+        <div className="card card1">
+          <div className="card__img"></div>
+          <p className="card__text">laksjdfkjsdlfjl</p>
+          <a className="card__link" href="#">
+            link
+          </a>
+        </div>
+
+        <div className="card card2">
+          <div className="card__img"></div>
+          <p className="card__text">laksjdfkjsdlfjl</p>
+          <a className="card__link" href="#">
+            link
+          </a>
+        </div>
+
+        <div className="card card3">
+          <div className="card__img"></div>
+          <p className="card__text">laksjdfkjsdlfjl</p>
+          <a className="card__link" href="#">
+            link
+          </a>
+        </div>
+
+        <div className="card card4">
+          <div className="card__img"></div>
+          <p className="card__text">laksjdfkjsdlfjl</p>
+          <a className="card__link" href="#">
+            link
+          </a>
+        </div>
+      </div>
     </div>
   );
 }

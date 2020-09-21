428da941b67f804c7765cc172484521fc39879fd
nathanmailhiot <nathanjm12@outlook.com>
2020-09-21 14:20:50 -0400

    Projects, animations for about

--- src/Components/PersonalProjects.js
+++ src/Components/PersonalProjects.js
@@ -6,7 +6,12 @@ export default function PersonalProjects() {
       <div className="projectsGrid">
         <div className="card card1">
           <div className="card__img"></div>
-          <p className="card__text">laksjdfkjsdlfjl</p>
+          <p className="card__text">
+            My first full stack project a to-do list! In this project I
+            developed a back end api using Ruby, Rails and PostgreSQL. It was
+            also my first project using Handlebars. Working on it was a great
+            experience!
+          </p>
           <a className="card__link" href="#">
             link
           </a>
@@ -14,7 +19,10 @@ export default function PersonalProjects() {
 
         <div className="card card2">
           <div className="card__img"></div>
-          <p className="card__text">laksjdfkjsdlfjl</p>
+          <p className="card__text">
+            This is a full stack workout application. The Front end consists of
+            React and the Back end is Node and MongoDB Atlas.
+          </p>
           <a className="card__link" href="#">
             link
           </a>
@@ -22,7 +30,10 @@ export default function PersonalProjects() {
 
         <div className="card card3">
           <div className="card__img"></div>
-          <p className="card__text">laksjdfkjsdlfjl</p>
+          <p className="card__text">
+            This is my first full stack app using React for the front end. It is
+            a monthly budgeting app. I used React and Rails.
+          </p>
           <a className="card__link" href="#">
             link
           </a>
@@ -30,7 +41,10 @@ export default function PersonalProjects() {
 
         <div className="card card4">
           <div className="card__img"></div>
-          <p className="card__text">laksjdfkjsdlfjl</p>
+          <p className="card__text">
+            This is a MERN stack application. It is used to look up all the
+            museum collectables on Animal Crossing New Horizons.
+          </p>
           <a className="card__link" href="#">
             link
           </a>

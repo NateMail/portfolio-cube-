af388b42a826c45847fe1cbb9d3aef3094e437cc
nathanmailhiot <nathanjm12@outlook.com>
2020-09-21 12:54:35 -0400

    Colors and Fonts

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,12 +6,17 @@ export default function About() {
       <div className="about">
         <h1 className="about__heading">Nate Mailhiot</h1>
         <p className="about__text">
-          I'm a software engineer from the Boston area. I previously have a
-          background working with special needs adults. I have completed a
-          Software Engineering bootcamp at General Assembly. I started my coding
-          journey back in October 2018. I started taking some Python classes
-          online and then decided to make it a career. I started at General
-          Assembly in March 2019 and finished in June 2019.
+          I'm a full stack software engineer from the Boston area. I previously
+          have a background working with special needs people for about 8 years.
+          My coding journey started back in October 2018. Coding went from a
+          hobby that I enjoyed to me considering making a career change farily
+          quickly. It led me to taking General Assembly's Software Engineering
+          Immersive bootcamp in 2019. Since then I have been working on
+          furthering my developer skill set. My area of expertise is working on
+          all things front end. Especially in React. Outside of coding I enjoy
+          fishing, playing guitar, hiking, being with friends, and so much more!
+          If you are looking for a landing page or hard working developer feel
+          free to contact me!
         </p>
         <div className="about__img"></div>
       </div>

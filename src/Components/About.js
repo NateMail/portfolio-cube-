a89d9a04b44c468ae81e32653723f199a1c2c337
nathanmailhiot <nathanjm12@outlook.com>
2020-10-02 17:21:29 -0400

    Spelling

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,15 +6,15 @@ export default function About() {
       <div className="about">
         <h1 className="about__heading">Nate Mailhiot</h1>
         <p className="about__text">
-          I'm a full stack software engineer from the Boston area. I previously
-          have a background working with special needs people for about 8 years.
-          My coding journey started back in October 2018. Coding went from a
-          hobby that I enjoyed to me considering making a career change farily
+          I am a full stack software engineer from the Boston area. My previous
+          background is working with special needs people. I did this for about
+          8 years. My coding journey started back in October of 2018. Coding
+          went from a hobby that I enjoy, to making a career change fairly
           quickly. It led me to taking General Assembly's Software Engineering
           Immersive bootcamp in 2019. Since then I have been working on
           furthering my developer skill set. My area of expertise is working on
           all things front end. Especially in React. Outside of coding I enjoy
-          fishing, playing guitar, hiking, being with friends, and so much more!
+          fishing, playing guitar, hiking, being with friends and so much more!
           If you are looking for a landing page or hard working developer feel
           free to contact me!
         </p>

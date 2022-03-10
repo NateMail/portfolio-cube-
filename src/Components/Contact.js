a559d4eba9e163267f505af8f9e0281ff2a02d77
Nathan Mailhiot <nathanjm12@outlook.com>
2022-03-10 12:05:00 -0500

    sarah Projects

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,7 +8,8 @@ export default function Contact() {
   const [userMessage, setUserMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  const reg = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const reg =
+    /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const { REACT_APP_EMAILJS_USERID, REACT_APP_EMAILJS_TEMPLATEID } = env;
 
--- src/Components/ProfessionalProjects.js
+++ src/Components/ProfessionalProjects.js
@@ -35,14 +35,17 @@ export default function ProfessionalProjects() {
         </div>
 
         <div className="card card3">
-          <div className="card__img professionalCardImg1"></div>
-          <p className="card__text">COMING SOON!</p>
-          {/* <a className="card__link" href="#">
+          <div className="card__img professionalCardImg3"></div>
+          <p className="card__text">
+            This website is a frontend react build for Peace of mind mentoring.
+            The website uses Sass, netlify, React, and email.js.
+          </p>
+          <a
+            className="card__link"
+            href="https://www.peaceofmindmentoring.com/"
+          >
             link
           </a>
-          <a className="card__repo" href="#">
-            repo
-          </a> */}
         </div>
 
         <div className="card card4">
